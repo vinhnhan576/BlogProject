689b890183e66577de9ddb1367a784c74e9816c6
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-07-24 00:15:34 +0700

    add

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
-const express = require('express');
-const router =  express.Router();
+const express = require("express");
+const router = express.Router();
 const db = require("db");
-
 
 module.exports = router;
