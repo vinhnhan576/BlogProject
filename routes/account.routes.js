a200b7f607539bf6d34b043f81ae63899a29cf4a
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-08-02 16:05:52 +0700

    update ORM Sequelize associations

--- routes/account.routes.js
+++ routes/account.routes.js
@@ -4,6 +4,12 @@ const express = require("express");
 const router = express.Router();
 
 //get all accounts
-router.get("/", accountController.getAccountList);
+router.get("/", accountController.getAllAccounts);
 
 //get account by id
+router.get("/:id", accountController.getAccountByID);
+
+//add new account
+router.post("/", accountController.addNewUser);
+
+module.exports = router;
