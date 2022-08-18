8af489229a667dba28ad59284f07bef14f8ca717
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-08-19 01:43:08 +0700

    update server

--- server/models/topic.js
+++ server/models/topic.js
@@ -1,26 +1,29 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Topic extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-        this.hasMany(models.Blog, { as: 'Blog', foreignKey: 'topicID' });
-    }
-  }
-  Topic.init(
-    {
-      topicName: DataTypes.STRING,
-      img: DataTypes.STRING,
-      maxim: DataTypes.STRING,
-    },
-    {
-      sequelize,
-      modelName: "Topic",
-    }
-  );
-  return Topic;
+	class Topic extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			this.hasMany(models.Blog, { as: "Blog", foreignKey: "topicID" });
+			this.belongsTo(models.User, { as: "User", foreignKey: "userID" });
+		}
+	}
+	Topic.init(
+		{
+			topicName: DataTypes.STRING,
+			img: DataTypes.STRING,
+			quote: DataTypes.STRING,
+			slug: DataTypes.STRING,
+			userID: DataTypes.INTEGER,
+		},
+		{
+			sequelize,
+			modelName: "Topic",
+		}
+	);
+	return Topic;
 };
--- server/service/blog.service.js
+++ server/service/blog.service.js
@@ -1,27 +1,44 @@
-const db = require('../models')
-const Op = require("sequelize")
+const db = require("../models");
+const Op = require("sequelize");
 
 let getAllBlogsByUserID = async (userID) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      let user = await db.Blog.findAll({
-        //include:[{Model:db.Topic}],
-        where: {
-          UserID: userID,
-        },
+	return new Promise(async (resolve, reject) => {
+		try {
+			let user = await db.Blog.findAll({
+				include: [
+					{
+						model: db.Topic,
+						as: "Topic",
+						include: [
+							{
+								model: db.User,
+								as: "User",
+							},
+						],
+						where: {
+							userID: userID,
+						},
+					},
+				],
 
-         attributes: [
-          'id','title', 'content','coverImg' // We had to list all attributes... // To add the aggregation...
-         ]
-      });
-      if (user) resolve(user);
-      resolve("Cannot find blog with id " + userID);
-    } catch (e) {
-      reject(e)
-    }
-  });
+				attributes: [
+					"id",
+					"title",
+					"coverImg",
+					"date",
+					"content",
+					"slug",
+					"topicID", // We had to list all attributes... // To add the aggregation...
+				],
+			});
+			if (user) resolve(user);
+			resolve("Cannot find user with id " + userID);
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 
 module.exports = {
-  getAllBlogsByUserID: getAllBlogsByUserID,
+	getAllBlogsByUserID: getAllBlogsByUserID,
 };
