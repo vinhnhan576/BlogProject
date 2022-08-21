ca4502a2e5444c6230c0de5867e40ee5e60cfcd7
sumori <hoangthiphuonguyen1602@gmail.com>
2022-08-21 12:49:26 +0700

    update category client

--- client/src/pages/Category.jsx
+++ client/src/pages/Category.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import Hello from "../assets/image/About/xinchao.png";
 import Helmet from "../components/Helmet";
+import CategoryCard from '../components/CategoryCard'
+import AllCategoryCards from "../components/AllCategoryCards";
+import Banner from '../components/Banner'
 
 function Category() {
-  return <Helmet title="Chủ đề">Category!!!</Helmet>;
+  return (
+    <div>
+      <Helmet title="Chủ đề">Category!!!</Helmet>;
+      <Banner img={Hello} />
+     <AllCategoryCards/>
+    </div>
+  );
 }
 
 export default Category;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
-
+import CategoryCard from '../components/CategoryCard'
 import Helmet from "../components/Helmet";
 import Topic from '../components/Topic';
+import Category from './Category';
 
 function Home() {
+  
   return (
     <div>
       <Helmet title="Trang chủ">Trang chủ</Helmet>
-      <Topic topicName="Đời sống"
+      <Topic topicName="Đời sống" />
+      <Topic topicName="Du lịch" />
+      <CategoryCard
+        //urlImage="https://cdn.pixabay.com/photo/2022/07/25/18/47/wat."
+        date="22/7/2022"
+        title="title1"
+        content="content1"
       />
     </div>
   );
