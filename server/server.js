72edb607cf249d46e1ee81d26f795f0db44d96f7
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-09-12 11:08:54 +0700

    fix heroku

--- server/server.js
+++ server/server.js
@@ -9,8 +9,8 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(upload.array());
-app.use(express.static("public"));
+// app.use(upload.array());
+// app.use(express.static("public"));
 
 connectDb();
 
