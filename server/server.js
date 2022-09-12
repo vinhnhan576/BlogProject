c3a675cfe1b4f9877e797976b8d1e2af2806063f
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-09-12 19:42:15 +0700

    fix heroku

--- server/server.js
+++ server/server.js
@@ -21,10 +21,7 @@ app.use("/api", apiRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
-app.use(
-	"/image",
-	express.static(path.join(__dirname, "./client/src/assets/image/blog"))
-);
+app.use("/blog", express.static(path.join(__dirname, "../image/blog")));
 
 app.get("/", (req, res) => {
 	res.send("server index page");
