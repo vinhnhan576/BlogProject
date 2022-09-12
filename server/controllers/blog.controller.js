fab692c2dde4b81358991cb609e31ac0327c39a3
vnhxn <99260394+vinhnhan576@users.noreply.github.com>
2022-09-12 19:53:56 +0700

    fix heroku

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -30,7 +30,7 @@ exports.deleteBlogByID = async (req, res) => {
 
 const storage = multer.diskStorage({
 	destination: (req, file, cb) => {
-		cb(null, "/");
+		cb(null, "image");
 	},
 	filename: (req, file, cb) => {
 		cb(
